dd55e84c9bb69e5f104556ce9390d12864a7ebe0
MiraD1n <mupolischuk@gmail.com>
2019-10-15 23:49:53 +0300

    add connect to DB

---
+++ dorel.zzz.com.ua/connect_db.php
@@ -0,0 +1,9 @@
+<?php
+try {
+  $pdo = new PDO ('mysql:host=127.0.0.1;dbname=dorel','dorel','Qq1234567',[PDO:ATTR_ERRMODE => PDO:ERRMODE_ECEPTION]);
+}
+catch (PDOExeption $e) {
+  echo "Ошибка выполнения запроса: " . $e->getMessage();
+}
+
+?>
