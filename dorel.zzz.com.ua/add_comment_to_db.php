7c8997820c0996ba1d80c23a154ef40d9f7dc81e
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-10-18 10:35:49 +0300

    work PDO

--- dorel.zzz.com.ua/add_comment_to_db.php
+++ dorel.zzz.com.ua/add_comment_to_db.php
@@ -1,6 +1,5 @@
 <?php
   require_once('connect_db.php');
   $username = $_POST['name'];
-  echo $username;
-  //$query = "INSERT INTO `coments` (`id`, `username`, `date`, `post`) VALUES (NULL, '' 123, NULL); "
+  $query = "INSERT INTO `coments` (`id`, `username`, `date`, `post`) VALUES (NULL, '' 123, NULL); "
 ?>
--- dorel.zzz.com.ua/connect_db.php
+++ dorel.zzz.com.ua/connect_db.php
@@ -1,9 +1,11 @@
 <?php
+
 try {
-  $pdo = new PDO ('mysql:host=localhost;dbname=dorel','dorel','Qq1234567','');
+  $pdo = new PDO('mysql:host=127.0.0.1;dbname=dorel','dorel','Qq1234567');
   echo "connection OK";
 }
 catch (PDOExeption $e) {
   echo "Невозможно соеденится";
 }
+
 ?>
