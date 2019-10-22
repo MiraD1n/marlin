bfd5269de19f1cba88b6a8ae7386e19466b03e70
MiraD1n <mupolischuk@gmail.com>
2019-10-22 23:30:28 +0300

    add redirection

--- dorel.zzz.com.ua/add_comment_to_db.php
+++ dorel.zzz.com.ua/add_comment_to_db.php
@@ -6,4 +6,5 @@
   $text = $_POST['text'];
   $query = "INSERT INTO `coments` (`id`, `username`, `date`, `post`) VALUES (NULL, '$username', '$date', '$text')";
   $insert = $pdo->query($query);
+  header('Location: /index.php')
 ?>
